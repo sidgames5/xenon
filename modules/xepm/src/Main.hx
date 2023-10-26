7cf7563e470e1e7be8e81e29d82a3813b1464bdd
Sid <saytleader@outlook.com>
2023-10-26 08:14:38 -0400

    Update Main.hx

--- modules/xepm/src/Main.hx
+++ modules/xepm/src/Main.hx
@@ -1,5 +1,17 @@
 class Main {
     static function main() {
-        
+        final args = Sys.args();
+        if (args.length < 2) {
+            Sys.println("Usage: xepm operation packages");
+            return;
+        }
+
+        final packages = args;
+        packages.shift();
+
+        switch (args[0]) {
+            case "install":
+            case "remove":
+        }
     }
 }
