84b23409e5dea2bd221811ff374d1f488547f6a9
Sid <saytleader@outlook.com>
2023-10-26 09:10:41 -0400

    Create Main.hx

---
+++ xe/src/Main.hx
@@ -0,0 +1,14 @@
+class Main {
+    static function main() {
+        final args = Sys.args();
+        final options = new Array<String>();
+        var operation = "";
+        final params = new Array<String>();
+
+        for (arg in args) {
+            if (arg.startsWith("--")) {}
+            else if (arg.startsWith("-")) {}
+            else {}
+        }
+    }
+}
