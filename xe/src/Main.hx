d3b47bec9d52aa92a167a8de613ca7f35d8bf5f4
Sid <saytleader@outlook.com>
2023-10-26 09:15:23 -0400

    Update Main.hx

--- xe/src/Main.hx
+++ xe/src/Main.hx
@@ -3,10 +3,17 @@ class Main {
         final args = Sys.args();
         final options = new Array<String>();
         var operation = "";
+        var module = "";
         final params = new Array<String>();
 
         for (arg in args) {
-            if (arg.startsWith("--")) {}
+            if (arg.startsWith("--")) {
+                switch (arg) {
+                    default:
+                        Sys.println('Unknown option: $arg');
+                        return;
+                }
+            }
             else if (arg.startsWith("-")) {}
             else {}
         }
