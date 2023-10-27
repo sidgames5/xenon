7b5fbcd95e4490c47c1e14d22463a4fb05b9b7fe
SidGames5 <saytleader@outlook.com>
2023-10-26 20:20:45 -0400

    xe

--- xe/src/Main.hx
+++ xe/src/Main.hx
@@ -1,53 +1,80 @@
-class Main {
-    static function main() {
-        final args = Sys.args();
-        final options = new Array<String>();
-        final operations = new Array<String>();
-        var module = "";
-        final params = new Array<String>();
+import sys.io.Process;
 
-        for (arg in args) {
-            if (arg.startsWith("--")) {
-                switch (arg) {
-                    default:
-                        Sys.println('Unknown option: $arg');
-                        return;
-                }
-            }
-            else if (arg.startsWith("-")) {
-                var opt = arg.substr(1).split("");
-                for (o in opt) {
-                    switch (o) {
-                        case "P":
-                            module = "xepm";
-                        case "C":
-                            module = "xepc";
-                        default:
-                            if (module == "P") {
-                                switch (o) {
-                                    case "i":
-                                        operations.push("install");
-                                    case "r":
-                                        operations.push("remove");
-                                    case "s":
-                                        operations.push("sync");
-                                    case "u":
-                                        operations.push("upgrade");
-                                }
-                            } else if (module == "C") {
-                                switch (o) {
-                                    case "e":
-                                        operations.push("export");
-                                    case "l":
-                                        operations.push("load");
-                                }
-                            }
-                    }
-                }
-            }
-            else {
-                params.push(arg);
-            }
-        }
-    }
+using StringTools;
+
+class Main {
+	public static final modpath = "/usr/lib/xenon";
+
+	static function main() {
+		final args = Sys.args();
+		final options = new Map<String, Dynamic>();
+		final operations = new Array<String>();
+		var module = "";
+		final params = new Array<String>();
+
+		var la = null;
+		for (arg in args) {
+			if (la != null) {
+				switch (la) {
+					case "root":
+						options.set("root", "arg");
+				}
+			} else if (arg.startsWith("--")) {
+				switch (arg.substr(2)) {
+					case "root":
+						la = "root";
+					default:
+						Sys.println('Unknown option: $arg');
+						return;
+				}
+			} else if (arg.startsWith("-")) {
+				var opt = arg.substr(1).split("");
+				for (o in opt) {
+					switch (o) {
+						case "P":
+							module = "xepm";
+						case "C":
+							module = "xepc";
+						default:
+							if (module == "P") {
+								switch (o) {
+									case "i":
+										operations.push("install");
+									case "r":
+										operations.push("remove");
+									case "s":
+										operations.push("sync");
+									case "u":
+										operations.push("upgrade");
+								}
+							} else if (module == "C") {
+								switch (o) {
+									case "e":
+										operations.push("export");
+									case "l":
+										operations.push("load");
+								}
+							}
+					}
+				}
+			} else {
+				params.push(arg);
+			}
+		}
+
+		switch (module) {
+			case "xepm":
+				var command = '$modpath/xepm ';
+				for (op in operations) {
+					switch (op) {
+						case "install":
+							command += "install " + params.join(" ");
+					}
+				}
+				Sys.command(command);
+			default:
+				Sys.println("Unsupported module");
+				return;
+		}
+	}
 }
